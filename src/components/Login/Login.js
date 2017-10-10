a04718087cd5150d62f5ce506e9b809fa20ebc48
mhutchison90 <michaelhutchison90@gmail.com>
2017-10-10 11:49:02 -0600

    more files

---
+++ src/components/Login/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Login.css';
+
+
+export default class Login extends Component {
+    render() {
+        return (
+            <div className='App'>  
+                <img src={logo} alt=""/>
+                <a href={ process.env.REACT_APP_LOGIN }><button>Login</button></a>
+            </div> 
+        )
+    }
+}
